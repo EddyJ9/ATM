ed719488a863291992da70f82198ebd2e1362de1
Eddy Jimenez <eduardojimenez9@yahoo.com>
2016-06-15 16:08:53 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * Created by EddyJ on 6/15/16.
+ */
+public class main {
+
+    public static void main(String[] args) throws Exception {
+        atm atm = new atm();
+        atm.chooseName();
+        atm.chooseOption();
+    }
+
+}
